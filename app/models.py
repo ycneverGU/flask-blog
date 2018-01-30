252d289a716a4f8060a778d3a0fb1f711560b445
ycngu <snl123698745@163.com>
2018-01-30 10:53:12 +0000

    add user page

--- app/models.py
+++ app/models.py
@@ -1,14 +1,26 @@
 from . import db, login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+
+
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     users = db.relationship('User', backref='role')
+    permissions = db.Column(db.Integer)
+    default = db.Column(db.Boolean, default=False, index=True)
 
     @staticmethod
     def seed():
@@ -16,25 +28,70 @@ class Role(db.Model):
                            'Guests', 'Administrators']))
         db.session.commit()
 
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT,
+                          Permission.WRITE, Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT,
+                              Permission.WRITE, Permission.MODERATE,
+                              Permission.ADMIN],
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    username = db.Column(db.String,unique=True,index=True)
     email = db.Column(db.String)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash=db.Column(db.String)
+    password_hash = db.Column(db.String)
     posts = db.relationship('Post', backref='author')
     comments = db.relationship('Comment', backref='author')
-    
-    last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
     confirmed = db.Column(db.Boolean, default=False)
     locale = db.Column(db.String, default='zh')
+    location = db.column(db.String)
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
 
     @staticmethod
     def on_created(target, value, oldvalue, initiator):
         target.role = Role.query.filter_by(name='Guests').first()
-    
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -55,16 +112,48 @@ class User(UserMixin, db.Model):
         db.session.add(self)
 
     def confirm(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'],3600)
+        s = Serializer(current_app.config['SECRET_KEY'], 3600)
         try:
             data = s.loads(token.encode('utf-8'))
-        except:
+        except BaseException:
             return False
         if data.get('confirm') != self.id:
             return False
         self.confirmed = True
         db.session.add(self)
+
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps(
+            {'change_email': self.id, 'new_email': new_email}).decode('utf-8')
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except BaseException:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
+        db.session.add(self)
         return True
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
+        return True
+
+
 class AnonymousUser(AnonymousUserMixin):
     @property
     def locale(self):
@@ -72,9 +161,17 @@ class AnonymousUser(AnonymousUserMixin):
 
     def is_administrator(self):
         return False
-    
+
     def id(self):
         return None
+
+    def can(self):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
 
 login_manager.anonymous_user = AnonymousUser
 
@@ -96,8 +193,6 @@ class Post(db.Model):
 
     comments = db.relationship('Comment', backref='post')
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-
 
 
 class Comment(db.Model):
